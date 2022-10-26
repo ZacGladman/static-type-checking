8d91ced19ccc4bbbd2cfae9ee9fd3a9e722570f2
Zac Gladman <zacharygladman@yahoo.co.uk>
2022-10-26 09:48:43 +0100

    :)

--- src/find-shortest-string.spec.ts
+++ src/find-shortest-string.spec.ts
@@ -3,10 +3,10 @@ import findShortestString from "./find-shortest-string";
 
 test("findShortestString finds the shortest string in an array", () => {
   expect(findShortestString(["it", "is", "a", "nice", "day"])).toBe("a");
-  expect(findShortestString(["why", "hello", "to", "you"])).toBe("to")
+  expect(findShortestString(["why", "hello", "to", "you"])).toBe("to");
 });
 
 test("findShortestString returns the earlier string in cases of joint shortest strings", () => {
   expect(findShortestString(["brave", "dance"])).toBe("brave");
-  expect(findShortestString(['!', '?'])).toBe('!')
+  expect(findShortestString(["!", "?"])).toBe("!");
 });
